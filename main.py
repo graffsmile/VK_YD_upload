3c3658965bda6ae69082c9a382465bc0fb70ccb8
graffsmile <sevulybin@yandex.ru>
2024-11-20 18:45:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from pprint import pprint
-
+import datetime
 import requests
 import os
 import json
 import os.path
 from dotenv import load_dotenv
+from tqdm import tqdm
+from time import sleep
+
 # import google.auth
 # from googleapiclient.discovery import build
 # from googleapiclient.errors import HttpError
@@ -39,7 +42,7 @@ def main():
         def _build_url(self, api_method):
             return f'{self.API_BASE_URL}/{api_method}'
 
-        def users_info(self):
+        def users_info(self, user_id: str):
             params = self.get_common_params()
             params.update({'user_ids': user_id})
             response = requests.get(self._build_url('users.get'), params=params)
@@ -52,7 +55,6 @@ def main():
             return response.json()
 
         def get_all_photos(self):
-            global size
             data = self.get_photos()
             all_photo_count = data['response']['count']
             i = 0
@@ -60,9 +62,9 @@ def main():
             photos = []
             max_size_photo = {}
 
-            # # Создаем папку на ПК для фото
-            if not os.path.exists(user_id):
-                os.mkdir(user_id)
+            # Создаем папку на ПК для фото
+            if not os.path.exists(f'id{user_id}_photo'):
+                os.mkdir(f'id{user_id}_photo')
 
             # Перебираем все фото
             while i <= all_photo_count:
@@ -70,32 +72,32 @@ def main():
                     data = self.get_photos(offset=i, count=count)
                 # Выбираем фото максимального размера и присваиваем фото имя по количеству лайков и записываем в словарь max_size_photos
                 for photo in data['response']['items']:
+                    photo['date'] = datetime.date.fromtimestamp(photo['date'])
                     max_size = 0
                     photos_info = {}
                     for size in photo['sizes']:
                         if size['height'] >= max_size:
                             max_size = size['height']
-                    # Проверяем, есть ли уже фото с таким же количеством лайков и, если есть добавляем к имени дату загрузки
+                    # Проверяем, есть ли уже фото с таким же количеством лайков и, если есть, добавляем к имени дату загрузки
                     if photo['likes']['count'] not in max_size_photo.keys():
                         max_size_photo[photo['likes']['count']] = size['url']
                         photos_info['file_name'] = f"{photo['likes']['count']}.jpg"
                     else:
-                        max_size_photo[f"{photo['likes']['count']} + {photo['date']}"] = size['url']
-                        photos_info['file_name'] = f"{photo['likes']['count']}+{photo['date']}.jpg"
+                        max_size_photo[f"{photo['likes']['count']} _ {photo['date']}"] = size['url']
+                        photos_info['file_name'] = f"{photo['likes']['count']} _ {photo['date']}.jpg"
 
-                    # Формируем список всех фотографий для дальнейшей упаковки в .json
+                    # Формируем список всех фотографий для дальнейшей упаковки в файл .json
                     photos_info['size'] = size['type']
                     photos.append(photos_info)
 
-                for photo_name, photo_url in max_size_photo.items():
-                    with open(f'{user_id}/%s' % f'{photo_name}.jpg', 'wb') as file:
+                for photo_name, photo_url in tqdm(max_size_photo.items(), desc='Total'):
+                    with open(f'id{user_id}_photo/%s' % f'{photo_name}.jpg', 'wb') as file:
                         img = requests.get(photo_url)
                         file.write(img.content)
-
-                print(f'Загружено {len(max_size_photo)} фото')
                 i += count
+            print(f'Загружено {len(max_size_photo)} фото')
 
-            # Записываем данные о всех скачанных фоторафиях в файл .json
+            # Записываем данные о всех скачанных фотографиях в файл photos.json
             with open("photos.json", "w") as file:
                 json.dump(photos, file, indent=4)
 
@@ -162,17 +164,18 @@ def main():
         uploader = GDUploader(gd_token)
         disk = 'Гугл диск'
 
-    folder_name = user_id
+    folder_name = f'id{user_id}_photo'
     uploader.folder_creation()
 
     photos_list = os.listdir(folder_name)
     count = 0
-    for photo in photos_list:
+    for photo in tqdm(photos_list, desc='Total'):
+        sleep(0.01)
         file_name = photo
         files_path = os.getcwd() + '\\' + folder_name +'\\' + photo
         result = uploader.upload(files_path)
         count += 1
-        print(f'На {disk} загружено фотографий: {count}')
+    print(f'На {disk} загружено фотографий: {count}')
 
 if __name__ == '__main__':
     main()
